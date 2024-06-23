f95a19fc72b91b64c90c75bec5c4fd2560837806
junsu0302 <junsu96378224@gmail.com>
2024-06-23 17:04:09 +0900

    # 리팩토링
    데이터 분류를 3개에서 2개로 수정

--- study/ML/data.py
+++ study/ML/data.py
@@ -14,49 +14,42 @@ labels -= labels.mean()
 features = np.linspace(-2, 2, len(labels))
 
 # 데이터를 훈련, 검증, 테스트 세트로 분할
-split_s = int(0.25 * len(features))
-split_e = int(0.25 * len(features))
+split = int(0.25 * len(features))
 
 index = np.arange(len(features))
 np.random.shuffle(index)
-index_test = np.sort(index[:split_s])
-index_validation = np.sort(index[split_s:split_s+split_e])
-index_train = np.sort(index[split_s+split_e:])
+index_test = np.sort(index[:split])
+index_train = np.sort(index[split:])
 
 f_test = features[index_test]
-f_validation = features[index_validation]
 f_train = features[index_train]
 l_test = labels[index_test]
-l_validation = labels[index_validation]
 l_train = labels[index_train]
 
-# 데이터 시각화
-fig, axs = plt.subplots(2, 2, figsize=(14, 10))
+def plot():
+  # 데이터 시각화
+  fig, axs = plt.subplots(1, 3, figsize=(20, 5))
 
-# 전체 데이터셋
-axs[0, 0].plot(features, labels, 'ro')
-axs[0, 0].set_title('Main Dataset')
-axs[0, 0].set_xlabel('features')
-axs[0, 0].set_ylabel('labels')
+  # 전체 데이터셋
+  axs[0].plot(features, labels, 'ro')
+  axs[0].set_title('Main Dataset')
+  axs[0].set_xlabel('features')
+  axs[0].set_ylabel('labels')
 
-# 훈련 데이터셋
-axs[0, 1].plot(f_train, l_train, 'go')
-axs[0, 1].set_title('Training Set')
-axs[0, 1].set_xlabel('features')
-axs[0, 1].set_ylabel('labels')
+  # 훈련 데이터셋
+  axs[1].plot(f_train, l_train, 'go')
+  axs[1].set_title('Training Set')
+  axs[1].set_xlabel('features')
+  axs[1].set_ylabel('labels')
 
-# 검증 데이터셋
-axs[1, 0].plot(f_validation, l_validation, 'bo')
-axs[1, 0].set_title('Validation Set')
-axs[1, 0].set_xlabel('features')
-axs[1, 0].set_ylabel('labels')
+  # 테스트 데이터셋
+  axs[2].plot(f_test, l_test, 'mo')
+  axs[2].set_title('Test Set')
+  axs[2].set_xlabel('features')
+  axs[2].set_ylabel('labels')
 
-# 테스트 데이터셋
-axs[1, 1].plot(f_test, l_test, 'mo')
-axs[1, 1].set_title('Test Set')
-axs[1, 1].set_xlabel('features')
-axs[1, 1].set_ylabel('labels')
+  plt.tight_layout()
+  plt.savefig('assets/ML/data.png')
+  plt.show()
 
-plt.tight_layout()
-plt.savefig('assets/ML/data.png')
-plt.show()
+# plot()
