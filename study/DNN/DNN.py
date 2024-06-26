65d227079186780e5a8c18308a66b47e9e2127ba
junsu0302 <junsu96378224@gmail.com>
2024-06-26 14:12:01 +0900

    #5 - 규제화를 추가한 학습
    드롭 아웃 방식에 규제화를 추가하여 과적합 방지
    
    Train Evaluate - Loss: 0.5684, Accuracy: 0.6829
     Test Evaluate - Loss: 0.6634, Accuracy: 0.5916

--- study/DNN/DNN.py
+++ study/DNN/DNN.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 from keras.src.layers import Dense, Dropout
 from keras.src.models import Sequential
 from keras.src.optimizers import Adam
+from keras.src.regularizers import L2
 from sklearn.metrics import accuracy_score
 
 random.seed(100)
@@ -57,14 +58,16 @@ class_weight = get_weight(data)
 
 # 모델 생성
 optimizer = Adam(learning_rate=0.001)
-def create_model(hidden_layer=2, units=128, dropout=True, rate=0.3, optimizer=optimizer):
+def create_model(hidden_layer=2, units=128, dropout=True, rate=0.3, regularize=True, reg=L2(0.001), optimizer=optimizer, input_dim=len(cols)):
+  if not regularize:
+    reg = None
   model = Sequential()
-  model.add(Dense(units, input_dim=len(cols), activation='relu'))
+  model.add(Dense(units, input_dim=input_dim, activity_regularizer=reg, activation='relu'))
 
   if dropout:
     model.add(Dropout(rate, seed=100))
   for _ in range(hidden_layer):
-    model.add(Dense(units, activation='relu'))
+    model.add(Dense(units, activation='relu', activity_regularizer=reg))
     if dropout:
       model.add(Dropout(rate, seed=100))
   model.add(Dense(1, activation='sigmoid'))
